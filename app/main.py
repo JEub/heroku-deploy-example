dfeae2d17abe06c8d857c2c9dd6bfc7b94a29dc3
James Eubanks <jeubanks.github@gmail.com>
2022-08-04 21:36:20 -0400

    adding mongo for stats tracking

--- app/main.py
+++ app/main.py
@@ -3,20 +3,38 @@ from flask_pymongo import PyMongo
 import random
 import requests
 
+import os
+
 
 app = Flask(__name__)
 
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+mongo = PyMongo(app)
 
 @app.route("/")
 def index():
     character = requests.get(f"https://swapi.dev/api/people/{random.randint(1,80)}/")
     character = character.json()
     
-    return render_template("index.html", character=character)
+    stats = load_and_get_stats(character)
+
+    return render_template("index.html", character=character, stats=stats)
 
 @app.route('/refresh')
 def refresh():
     return redirect('/')
+
+@app.route('/clear')
+def clear():
+    mongo.db.displaystats.delete_many({})
+    return redirect('/')
+
+def load_and_get_stats(body: dict) -> dict:
+    chars = mongo.db.displaystats
+
+    chars.update_one({'name': body['name']}, {'$inc':{'displayed': 1}}, upsert=True)
+
+    return chars.find()
 
 if __name__ == "__main__":
     app.run(debug=True, port=5001)
